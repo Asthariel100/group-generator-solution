ecc54bc3a0959b892dfdada10fd32da821498579
Yohann <yohann.fallas25@gmail.com>
2021-12-16 11:23:24 +0100

    all good

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+
+function addParticipant(event) {
+    event.preventDefault();
+    const nameInputElt = document.getElementById('nameInput')
+    const participantName = nameInputElt.value.trim();
+    console.log("test")
+    if(participantName === ""){
+        alert("le Nom est obligatioire !!");
+        return;
+    }    
+    const participantElt = `<li>${participantName}</li>   `;
+    const participantListElt = document.getElementById("participantList");
+    participantListElt.innerHTML = participantListElt.innerHTML + participantElt;
+    nameInputElt.value = "";  
+    
+    
+}
+const addNameformElt = document.getElementById("addNameForm");
+addNameformElt.addEventListener('submit', addParticipant);
